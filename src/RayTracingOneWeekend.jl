cc1306b35dfb231b07100e6fc4d4bc85dd5f82d9
Alok Singh <alokbeniwal@gmail.com>
2022-11-12 15:12:10 -0800

    Update

--- src/RayTracingOneWeekend.jl
+++ src/RayTracingOneWeekend.jl
@@ -1,5 +1,29 @@
-module RayTracingOneWeekend
+import FromFile: @from
+using Grassmann
 
-# Write your package code here.
+const PPM_FILE = "/Users/alokbeniwal/.julia/dev/RayTracingOneWeekend/target/raytraced.ppm"
 
+
+function rand_img()
+    rand_color() = rand(0:255)
+    img = map(_ -> (rand_color(), rand_color(), rand_color()), zeros(256, 256))
+    img
 end
+
+function write_ppm(matrix, filename = PPM_FILE)
+    rows, cols = size(matrix)
+    open(filename, "w") do io
+        write(
+            io,
+            """
+      P3
+      $(cols) $(rows)
+      """,
+        )
+        for (r, g, b) in matrix # col major reading, vertical
+            write(io, "$r $g $b \n")
+        end
+    end
+end
+
+write_ppm(rand_img())
