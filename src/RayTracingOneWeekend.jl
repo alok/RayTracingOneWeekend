83ac2deeb88836f170823c31e6bb37b272f056f2
Alok Singh <alokbeniwal@gmail.com>
2022-11-13 12:30:35 -0800

    use Option type

--- src/RayTracingOneWeekend.jl
+++ src/RayTracingOneWeekend.jl
@@ -15,6 +15,13 @@ struct Sphere{R <: Real} <: Solid
     radius::R
 end
 
+struct HitRecord
+    pt::Point
+    normal::Point
+    t::Float64
+    front_face::Bool
+end
+
 # Ray = Tuple{Point, Point} # start, end
 struct Ray
     origin::Point
@@ -26,9 +33,9 @@ at(ray, t) = Ray(ray.origin, t * ray.direction)
 # TODO: each ray can be done totally parallel to the others
 function color(ray::Ray)
     # hardcode test for sphere
-    t = hit(Sphere(Point(0, 0, -1), 0.5), ray)
-    if t > 0
-        N = normalize(at(ray, t).direction - Point(0, 0, -1))
+    hit_record = hit(Sphere(Point(0, 0, -1), 0.5), ray)
+    if hit_record !== nothing && hit_record.t > 0
+        N = normalize(at(ray, hit_record.t).direction - Point(0, 0, -1))
         return Color(N .+ 1) ./ 2
     end
     t = 0.5 * (ray.direction[2] + 1.0)
@@ -51,18 +58,49 @@ end
 # TODO: can broadcast to color in parallel
 function color(rays) end
 
-
-function hit(sphere::Sphere, ray::Ray)
+# return record or nothing
+function hit(sphere::Sphere, ray::Ray, t_min = -Inf, t_max = Inf)::Union{HitRecord, Nothing}
     oc = ray.origin - sphere.center
     a = normsq(ray.direction)
     half_b = oc ⋅ ray.direction
     c = normsq(oc) - sphere.radius^2
     discriminant = half_b^2 - a * c
     if discriminant < 0
-        return -1 # why?
-    else
-        -(half_b + sqrt(discriminant)) / a
+        return nothing
     end
+
+    root = -(half_b + sqrt(discriminant)) / a
+    if root < t_min || root > t_max
+        # try other root
+        root = -(half_b - sqrt(discriminant)) / a
+        if root < t_min || root > t_max
+            return nothing
+        end
+    end
+
+    pt = at(ray, root).origin
+    normal = (pt - sphere.center) / sphere.radius
+    front_face = normal ⋅ ray.direction < 0
+
+    HitRecord(pt, front_face ? normal : -normal, root, front_face)
+end
+
+function hit(
+    xs::AbstractArray{<:Solid},
+    ray::Ray,
+    t_min = -Inf,
+    t_max = Inf,
+)::Union{HitRecord, Nothing}
+    closest = nothing
+    closest_t = t_max
+    for x in xs
+        hit_record = hit(x, ray, t_min, closest_t)
+        if hit_record !== nothing && hit_record.t < closest_t
+            closest = hit_record
+            closest_t = hit_record.t
+        end
+    end
+    closest
 end
 
 # TODO: intersect ray and viewport (line meet bivector)
